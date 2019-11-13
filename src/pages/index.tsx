a4453b1b32b62e725c2f8908b44953f36f321fad
jinchengli <jinchengli@wxblockchain.com>
2019-11-13 14:53:30 +0800

    init init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,15 @@
+import Link from 'umi/link';
 import React from 'react';
+
 import styles from './index.css';
-import { formatMessage } from 'umi-plugin-locale';
-export default function() {
+
+export default function () {
   return (
     <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            {formatMessage({ id: 'index.start' })}
-          </a>
-        </li>
-      </ul>
+      <h1>Page index</h1>
+      <div><Link to="./users">go to /users</Link></div>
+      <div><Link to="./graph/index">go to /Graph</Link></div>
     </div>
   );
 }
+
