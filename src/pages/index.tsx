94d4e4db47f535b8e82cab72aa907b27a1f694dd
jinchengli <jinchengli@wxblockchain.com>
2021-12-07 12:00:00 +0800

    del unless router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+/*
+ * @Date: 2021-12-06 11:31:51
+ * @LastEditors: jcl
+ * @LastEditTime: 2021-12-07 11:59:18
+ * @FilePath: /umi-app/src/pages/index.tsx
+ */
 import Link from 'umi/link';
 import React from 'react';
 
@@ -9,7 +15,6 @@ export default function () {
       <h1>Page index</h1>
       <div><Link to="./users">go to /users</Link></div>
       <div><Link to="./graph/index">go to /Graph</Link></div>
-      <div><Link to="./graph/demo/index">go to demo/Graph</Link></div>
     </div>
   );
 }
