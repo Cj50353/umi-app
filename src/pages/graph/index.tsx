4b653eb96a3cd83741fefc9a20c516ab9e94afb8
jinchengli <jinchengli@wxblockchain.com>
2021-12-07 10:43:58 +0800

    description

--- src/pages/graph/index.tsx
+++ src/pages/graph/index.tsx
@@ -1,9 +1,16 @@
+/*
+ * @Date: 2021-12-06 11:31:51
+ * @LastEditors: jcl
+ * @LastEditTime: 2021-12-07 10:39:53
+ * @FilePath: /umi-app/src/pages/graph/index.tsx
+ */
 import * as React from 'react';
 import styles from './index.css';
 import TransferTree from "./components/transferTree"
+//@ts-ignore
 import { data } from './data'
 
-export default ()=>{
+export default () => {
   return <div className={styles.normal}>
     <TransferTree data={data} />
   </div>
