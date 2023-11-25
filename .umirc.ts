87b7a9b3d92bcf900bf5ce2a42975b7b8ffef06a
jinchengli <691068928@qq.com>
2023-11-25 21:07:08 +0800

    init

--- .umirc.ts
+++ .umirc.ts
@@ -1,40 +1,9 @@
-import { IConfig } from 'umi-types';
+import { defineConfig } from "umi";
 
-// ref: https://umijs.org/config/
-const config: IConfig =  {
-  treeShaking: true,
+export default defineConfig({
   routes: [
-    {
-      path: '/',
-      component: '../layouts/index',
-      routes: [
-        { path: '/', component: '../pages/index' }
-      ]
-    }
+    { path: "/", component: "index" },
+    { path: "/docs", component: "docs" },
   ],
-  plugins: [
-    // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: { webpackChunkName: true },
-      title: 'umi-app',
-      dll: true,
-      locale: {
-        enable: true,
-        default: 'en-US',
-      },
-      routes: {
-        exclude: [
-          /models\//,
-          /services\//,
-          /model\.(t|j)sx?$/,
-          /service\.(t|j)sx?$/,
-          /components\//,
-        ],
-      },
-    }],
-  ],
-}
-
-export default config;
+  npmClient: 'npm',
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import styles from './index.css';
-import { formatMessage } from 'umi-plugin-locale';
-export default function() {
+import yayJpg from '../assets/yay.jpg';
+
+export default function HomePage() {
   return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            {formatMessage({ id: 'index.start' })}
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h2>Yay! Welcome to umi!</h2>
+      <p>
+        <img src={yayJpg} width="388" />
+      </p>
+      <p>
+        To get started, edit <code>pages/index.tsx</code> and save to reload.
+      </p>
     </div>
   );
 }
